b8099983502248d0178fd58d3908cc2685052e35
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-17 09:54:20 -0300

    Update main

--- main.c
+++ main.c
@@ -7,12 +7,20 @@ int main(void) {
   int count = 0;
 
   printf("Circular queue\n");
-  int choice = 1;
   
-  while (choice != 0) {
+  
+  int choice;
+  do {
+    printf("\n1. Agregar un elemento");
+    printf("\n2. Acceder a un elemento");
+    printf("\n3. Imprimir la lista");
+    printf("\n0. Salir");
+    printf("\nSu eleccion: ");
+    scanf(" %d", &choice);
+
     switch (choice) {
       case 1:
-        content = push(front, rear, &count);
+        content = push(&front, &rear, &count);
 
         if (content != NULL) {
           printf("Agregaste un elemento con el contenido: %s\n\n",
@@ -33,7 +41,7 @@ int main(void) {
         printf("\nUps! Parece que elegiste una opcion incorrecta\n\n");
       break;
     }
-  }
+  } while (choice != 0);
 
   return EXIT_SUCCESS;
 }
