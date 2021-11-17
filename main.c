7985795486c0fceaa3b3f58717abbbcacf1adbba
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-17 10:34:49 -0300

    Update main

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ int main(void) {
   
   int choice;
   do {
-    printf("\n1. Agregar un elemento");
-    printf("\n2. Acceder a un elemento");
-    printf("\n3. Imprimir la lista");
-    printf("\n0. Salir");
+    printf("\n\t1. Agregar un elemento");
+    printf("\n\t2. Acceder a un elemento");
+    printf("\n\t3. Imprimir la lista");
+    printf("\n\t0. Salir");
     printf("\nSu eleccion: ");
     scanf(" %d", &choice);
 
@@ -36,6 +36,10 @@ int main(void) {
       //       rear -> data);
       //   }
       // break;
+
+      case 3:
+        printList(front);
+      break;
     
       default:
         printf("\nUps! Parece que elegiste una opcion incorrecta\n\n");
@@ -44,4 +48,24 @@ int main(void) {
   } while (choice != 0);
 
   return EXIT_SUCCESS;
+}
+
+void printList(Node *front) {
+  Node *current = front;
+
+  printf("\nImprimir los elementos de la lista circular");
+  if (front == NULL) {
+    printf("\nLa lista esta vacia\n");
+  } else {
+    int count = 0;
+    
+    do {
+      printf("\n%d. El contenido es: %s", count, current->data);
+
+      current = current -> next;
+      count++;
+    } while (current != front);
+  }
+
+  printf("\n\n");
 }
