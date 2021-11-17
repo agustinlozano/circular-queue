baea8fcaea8e4f53e2dbe41f0b9f5c592e4c2ff1
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-17 12:32:27 -0300

    Edit function: printList

--- printList.c
+++ printList.c
@@ -3,19 +3,19 @@
 void printList(Node *front) {
   Node *current = front;
 
-  printf("\nImprimir los elementos de la lista circular");
   if (front == NULL) {
     printf("\nLa lista esta vacia\n");
+
   } else {
     int count = 0;
     
     do {
-      printf("\n%d. El contenido es: %s", count, current->data);
+      printf("\n%d. El contenido es: %s", count+1, current->data);
 
       current = current -> next;
       count++;
     } while (current != front);
   }
 
-  printf("\n\n");
+  printf("\n");
 }
